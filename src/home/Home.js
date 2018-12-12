6f6d8aec275fe3c35b14c0cd4e43b0a551995604
Hugh O'Neill <hugh.oneill.4@gmail.com>
2018-12-12 10:14:28 -0700

    change banner

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import './Home.css'
 import banner from '../graphics/home_banner.png'
 import banner2 from '../graphics/IntoTheLight02.jpg'
+import banner3 from '../graphics/IntoTheLightBanner.svg'
 import videos from './videosList'
 import {VideoTile} from '../components/VideoTile'
 
@@ -12,11 +13,11 @@ export class Home extends Component{
     render(){
         return (
             <div className="home-container">
-                <img id="home-title-image" src={banner2} alt="two people riding electric wheel in front of bridge"/>
+                <img id="home-title-image" src={banner3} alt="two people riding electric wheel in front of bridge"/>
                 <div id="banner-title" >
-                    <div id="banner-title-text">
+                    {/* <div id="banner-title-text">
                         yegwheel
-                    </div>
+                    </div> */}
                 </div>
                 <div id="home-about">
                     <div className="home-about-container" id="home-about-main">
@@ -61,14 +62,7 @@ export class Home extends Component{
                             10:45 Departure for road trip around city<br/>
                             <a href="https://goo.gl/maps/4xBd2gERt8P2" target="_blank">7727 98 St NW, Edmonton, AB T6E 5C9</a>
                         </Typography>
-                        {/* <iframe 
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2372.4542184049506!2d-113.48464598397383!3d53.51394436984508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a0220bab4886a5%3A0x29890ad07081c42d!2s7727+98+St+NW%2C+Edmonton%2C+AB+T6E+5C9!5e0!3m2!1sen!2sca!4v1544381872229" 
-                            width="400" 
-                            height="300" 
-                            frameBorder="0" 
-                            style={{border:0}} 
-                            allowFullScreen
-                        /> */}
+
                     </div>
                 </div>
                 <div id="home-video-container">
