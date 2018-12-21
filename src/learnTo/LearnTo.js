e83086e49408bc255cf219ff84db76eaa4b1ff3b
Hugh O'Neill <hugh.oneill.4@gmail.com>
2018-12-21 11:29:14 -0700

    learntobanner change title type

--- src/learnTo/LearnTo.js
+++ src/learnTo/LearnTo.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './LearnTo.css'
 
 import learningPic from '../graphics/learning_net_crop.jpg'
+import learningPic2 from '../graphics/LearnToRideBannerLC.svg'
+import learningPic3 from '../graphics/LearnToRideBannerLCitalics.svg'
 import videos from './learnToVideoList'
 import {VideoTile} from '../components/VideoTile'
 import {MUIVideoTile} from './MUIVideoTile'
@@ -10,12 +12,12 @@ export class LearnHowTo extends Component{
     render(){
         return(
             <div class="learn-to-container">
-                <img id="ltw-header-image" src={learningPic} alt="two people learning to use a wheel"/>
-                <div id="ltw-header-title">
+                <img id="ltw-header-image" src={learningPic3} alt="two people learning to use a wheel"/>
+                {/* <div id="ltw-header-title">
                     <div id="ltw-header-title-text">
                         Learn to Ride
                     </div>
-                </div>
+                </div> */}
                 <div id="ltw-content-container">
                 {
                     videos.map(vid => 
