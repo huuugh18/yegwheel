02dcc2180cb9a769cc3a87c49bb1a4133db61661
Terence O'Neill <terry.oneill@trystal.net>
2019-03-24 10:56:32 -0600

    here either

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,26 +1,5 @@
 import React from 'react';
 import './Home.css'
-<<<<<<< HEAD
-import trythis from '../graphics/Montage.png'
-import KS18XLCard from '../components/productcards/KS18XLCard'
-import KS16SCard from '../components/productcards/KS16SCard'
-import KS14DCard from '../components/productcards/KS14DCard'
-import AboutSection from '../components/aboutcards/AboutSection'
-import VideoSection from '../components/videocards/VideoSection'
-
-const FeatureImage = () => <img id="home-title-image" src={trythis} alt="two people riding electric wheel in front of bridge" style={{width:1366,height:768}}/>
-
-export const Home = () => <div className="home-container">
-  <FeatureImage />
-  <VideoSection />
-  <div id="home-purchase-container" style={{backgroundColor:'white', display:'flex', margin:20, justifyContent:'space-evenly'}}>
-    <KS14DCard />
-    <KS16SCard />
-    <KS18XLCard />
-  </div>
-  <AboutSection />
-</div>
-=======
 import './DisplayCard.css'
 import wheel14 from '../graphics/ks14d.png'
 import wheel16 from '../graphics/ks16s.png'
@@ -107,6 +86,5 @@ export class Home extends Component{
         )
     }
 }
->>>>>>> purchaseDivChange
 
 export default Home;
