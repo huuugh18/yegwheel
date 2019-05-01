a13d5a34f4a648e2efd7fb8cefe62058049654b3
Terence O'Neill <terry.oneill@trystal.net>
2019-05-01 06:11:22 -0600

    switch to deploy uris in auth0

--- src/Auth/authvars.js
+++ src/Auth/authvars.js
@@ -1,6 +1,6 @@
 export const domain = 'yegwheel.auth0.com'
 export const clientID = 'A9IKn80Sg1cSGC3RnSX2kDlj2omnU8Jd'
 export const name = 'yegwheelSPA'
-export const redirectUri = 'http://localhost:3000/callback'
-export const logoutUri = 'http://localhost:3000'
+export const redirectUri = 'http://yegwheel.com/callback'
+export const logoutUri = 'http://yegwheel.com'
 
